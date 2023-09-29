9bf54817ac623965675398e9ea0cfbd5a0efdd17
jeremyito07 <nagwalanshu@gmail.com>
2023-09-29 20:24:56 +0530

    footer updated

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import {FaFacebook, FaInstagram, FaXTwitter, FaBehance} from 'react-icons/fa6'
 const Footer = () => {
 
   return (
-    <div>
+    <div className='md:hidden block'>
         <ul className='flex py-4 gap-6 px-4 bg-gray-900'>
   <li className='border mx-auto p-2 rounded-full text-white hover:bg-blue-700 cursor-pointer duration-300' title='Facebook'><FaFacebook size={30}/></li>
   <li className='border mx-auto p-2 rounded-full text-white hover:bg-gradient-to-r from-fuchsia-500 to-pink-500 duration-300 cursor-pointer' title='instagram'><FaInstagram size={30} /></li>
