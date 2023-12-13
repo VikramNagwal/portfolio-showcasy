1eb45eeed271d583adcdfb37375c43bdd4f60a96
jeremyito07 <nagwalanshu@gmail.com>
2023-12-13 17:56:52 +0530

    feat:Added Insights to Application

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Home from "./page/Home"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const App = () => {
 
   return (
     <div>
     <Home />
+    <SpeedInsights/>
     </div>
   )
 }
